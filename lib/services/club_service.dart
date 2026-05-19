1e239b86ee28ed03b0385ce184a5d192b682cde8
Joao-Salvalagio <joaosalvalagio@gmail.com>
2026-05-19 20:25:57 -0300

    Aula 19-05-2026 finalizada!

--- lib/services/club_service.dart
+++ lib/services/club_service.dart
@@ -1,3 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutter_beachup/models/club_model.dart';
+import 'package:http/http.dart' as http;
+
 class ClubService {
-  
+  final String baseUrl = "https://mocki.io/v1/51f3adb6-fffb-4b2b-981f-7b65e9c1322d";
+
+  Future<List<ClubModel>> getClubs() async{
+    var response = await http.get(
+      Uri.parse(baseUrl),
+      headers: {"Content-Type": "application/json"}
+    );
+
+    if(response.statusCode >= 200 && response.statusCode < 400){
+        List<dynamic> jsonList = jsonDecode(response.body);
+    }
+  }
 }
